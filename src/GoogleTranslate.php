f38bcdb9c9ee49454392ba2c01a11b4295ad0eef
crazy-hunk <61078222+crazy-hunk@users.noreply.github.com>
2020-04-13 00:11:36 +0530

    Create GoogleTranslate.php

---
+++ src/GoogleTranslate.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*
+ * (c) Crazy Hunk <contact.crazyhunk@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace crazyhunk\GoogleTranslate;
+
+class GoogleTranslate
+{
+    public function say($toSay = "Nothing given")
+    {
+        return $toSay;
+    }
+}
